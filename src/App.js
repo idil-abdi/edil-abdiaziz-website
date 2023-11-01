ba6039f2ff882b9c2f0e55447f32e9a5b5890271
Edil Abdiaziz <edilabdiaziz@gmail.com>
2023-11-01 19:11:13 +0000

    website portfolio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container, Stack } from "react-bootstrap"
+import { Header } from "./components/Header"
+import { Profile } from "./components/Profile"
+import { Experience } from "./components/Experience"
+import { Projects } from "./components/Projects"
+import { Footer } from "./components/Footer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+    return (
+        <Container>
+            <Stack gap={5}>
+                <Header/>
+                <Profile/>
+                <Experience/>
+                <Projects/>
+                <Footer/>
+            </Stack>
+        </Container>
+    )
 }
-
-export default App;
---
+++ src/components/Experience.js
@@ -0,0 +1,28 @@
+import { Stack } from "react-bootstrap"
+
+export const Experience = () => {
+    return(
+        <Stack className='border rounded p-2'>
+            <h2 className="text-center">Experience</h2>
+            <Stack gap={4}>
+                    <div className="border rounded mx-5 px-5">
+                        <h3>Front-End</h3>
+                        <ul>
+                            <li>HTML<p>Experience</p></li>
+                            <li>CSS<p>Experience</p></li>
+                            <li>JavaScript<p>Intermediate</p></li>
+                        </ul>
+                    </div>
+                    <div className="border rounded mx-5 px-5">
+                        <h3>Back-End</h3>
+                        <ul>
+                            <li>Node JS<p>Beginner</p></li>
+                            <li>Git<p>Beginner</p></li>
+                            <li>React<p>Intermediate</p></li>
+                        </ul>
+                    </div>
+                
+            </Stack>
+        </Stack>
+    )
+}
